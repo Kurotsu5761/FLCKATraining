7a132c7b35206781ab72680a1bffe76c47039628
Kurox <hsiangweih5761@gmail.com>
2024-07-28 13:15:42 +0800

    remove private key output

--- terraform/ssh.tf
+++ terraform/ssh.tf
@@ -22,7 +22,3 @@ resource "azapi_resource" "ssh_public_key" {
 output "key_data" {
   value = azapi_resource_action.ssh_public_key_gen.output.publicKey
 }
-
-output "private_key" {
-  value = azapi_resource_action.ssh_public_key_gen.output.privateKey
-}
